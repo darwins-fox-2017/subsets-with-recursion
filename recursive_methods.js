0f9da35627a48c5510340b6fd46a3d55e7934b4e
mrhandoko <bambank.handoko@yahoo.com>
2017-02-06 01:51:03 +0700

    Subset with Recursion Done

--- recursive_methods.js
+++ recursive_methods.js
@@ -1,5 +1,17 @@
+let teamRange = (a, b) => {
+  var total = a, team = a;
+
+  while (team++ < b) { total *= team; }
+  return total;
+}
+
 let choose_team = (n, k) => {
-  //write your code here
+  if (n == k) {
+    return 1;
+  } else {
+    k = (k < n-k) ? n-k : k;
+    return teamRange(k+1, n)/teamRange(1,n-k);
+  }
 }
 
 console.log(choose_team(6, 2));
